94a4af94416f95f7a435bcec07b6a97e83ca9649
joy016 <jhoylejas@gmail.com>
2023-08-30 20:24:07 +0800

    added cards for restaurant

---
+++ src/app/components/cards/RestaurantCard.tsx
@@ -0,0 +1,55 @@
+import { AiFillStar } from 'react-icons/ai';
+import { MENU_DATA } from '../../../../const/restaurant-data';
+import styles from './restaurant-card.module.css';
+
+export default function RestaurantCard() {
+  return (
+    <div className={styles['cards-container']}>
+      {MENU_DATA.map(
+        ({
+          id,
+          imgUrl,
+          reviews,
+          restoName,
+          country_food,
+          location,
+          timesOfBook,
+        }) => (
+          <div
+            key={id}
+            className={`${styles['card']} ${styles['card--hover']}`}
+          >
+            <img src={imgUrl} className={styles['card__image']} />
+            <div className={styles['card__details-container']}>
+              <h3 className={styles['card__resto-name']}>{restoName}</h3>
+              <div className={styles['card__reviews-container']}>
+                <div>
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                  <AiFillStar />
+                </div>
+                <p
+                  className={styles['card__resto-reviews']}
+                >{`${reviews} reviews`}</p>
+              </div>
+
+              <div className={styles['card__details-location-container']}>
+                <p className={styles['card__resto-country-food']}>
+                  {country_food}
+                </p>
+                <p className={styles['card__resto-dollar-sign']}>$$$$</p>
+                <p className={styles['card__resto-location']}>{location}</p>
+              </div>
+
+              <p className={styles['card__resto-times-of-book']}>
+                {timesOfBook}
+              </p>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,11 @@
-import Image from 'next/image';
-import styles from './page.module.css';
 import Search from './search/Search';
-import { MENU_DATA } from '../../const/restaurant-data';
+import RestaurantCard from './components/cards/RestaurantCard';
 
 export default function Home() {
   return (
     <main>
       <Search />
-      <div className={styles['cards-container']}>
-        {MENU_DATA.map(
-          ({
-            id,
-            imgUrl,
-            reviews,
-            restoName,
-            country_food,
-            location,
-            timesOfBook,
-          }) => (
-            <div key={id} className={styles['restaurant-card']}>
-              <img src={imgUrl} />
-              <h3>{restoName}</h3>
-              <p>{`${reviews} reviews`}</p>
-              <p>{country_food}</p>
-              <p>{location}</p>
-              <p>{timesOfBook}</p>
-            </div>
-          )
-        )}
-      </div>
+      <RestaurantCard />
     </main>
   );
 }
