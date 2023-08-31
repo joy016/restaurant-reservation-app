d05090f950ba5aa18bc57096dcef4f2bbed1ab52
joy016 <jhoylejas@gmail.com>
2023-08-31 23:25:47 +0800

    Implemented Overview Page

---
+++ const/imageUrl.ts
@@ -0,0 +1,20 @@
+const FOOD_IMAGE = [
+  {
+    id: 1,
+    imageSrc: '/images/food2.jpg',
+  },
+  {
+    id: 2,
+    imageSrc: '/images/food3.jpg',
+  },
+  {
+    id: 3,
+    imageSrc: '/images/food4.jpg',
+  },
+  {
+    id: 4,
+    imageSrc: '/images/food5.jpg',
+  },
+];
+
+export { FOOD_IMAGE };
