b2b411a8fec1540cbc1228825f27c45557d1d9c2
joy016 <jhoylejas@gmail.com>
2023-09-01 17:14:35 +0800

    added styles in reservation component

--- src/app/restaurant/[id]/components/reservation-card/ReservationCard.tsx
+++ src/app/restaurant/[id]/components/reservation-card/ReservationCard.tsx
@@ -1,22 +1,47 @@
+import styles from './reservation-card.module.css';
+
 export default function ReservationCard() {
   return (
     <>
       <h3>Make a Reservation</h3>
-      <p>Party size</p>
-      <select name="person" id="person">
+      <p className={styles['restaurant__reservation-partysize']}>Party size</p>
+      <select
+        name="person"
+        id="person"
+        className={styles['restaurant__reservation-personpicker']}
+      >
         <option value="1">1 Person</option>
         <option value="2">2 Persons</option>
         <option value="3">3-5 Persons</option>
         <option value="4">More than 10 Persons</option>
       </select>
-      <div>
-        <p>Date</p>
-        <p>Time</p>
+      <div className={styles['restaurant__reservation-datetime']}>
+        <p className={styles['restaurant__reservation-date']}>Date</p>
+        <p className={styles['restaurant__reservation-time']}>Time</p>
       </div>
       <div>
-        <input type="date" id="birthday" name="birthday"></input>
+        <input
+          type="date"
+          id="birthday"
+          name="birthday"
+          className={styles['restaurant__reservation-datepicker']}
+        />
+        <select
+          className={styles['restaurant__reservation-timepicker']}
+          name="person"
+          id="person"
+        >
+          <option value="1">1-2 PM</option>
+          <option value="2">5-10 PM</option>
+          <option value="3">10-11 PM</option>
+          <option value="4">12-1PM</option>
+        </select>
       </div>
-      <button>Find a Time</button>
+      <button
+        className={`${styles['restaurant__reservation-button']} ${styles['restaurant__reservation-button--hover']}`}
+      >
+        Find a Time
+      </button>
     </>
   );
 }
